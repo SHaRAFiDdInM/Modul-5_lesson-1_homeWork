d0737b8c9a168cb2e89bf861970408a851311c8b
MShArAFidDiNM <sharafiddin2007@outlook.com>
2023-09-13 13:08:29 +0500

    +

---
+++ Category.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Supermarket
+{
+    internal class Category
+    {
+        public int Id  { get; set; }
+        public string CategoryName { get; set; }
+        public int NUmberOfProduct { get; set; }
+        public Category(int id,string categoryname,int number) 
+        {
+           Id = id;
+           CategoryName = categoryname;
+            NUmberOfProduct = number;
+        }
+
+    }
+}
---
+++ Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Supermarket
+{
+    internal class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int CategpryID { get; set; }
+        public Product(int id, string name, decimal price, int categpryID)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+            CategpryID = categpryID;
+        }
+    }
+}
